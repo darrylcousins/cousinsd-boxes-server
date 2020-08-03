8fc341c1571a2771164aab4e1740e86ddec03544
Darryl Cousins <darryljcousins@gmail.com>
2020-08-03 21:15:19 +1200

    added switch for production next config

--- Heroku.md
+++ Heroku.md
@@ -1,12 +1,12 @@
 # Installing on Heroku
 
-url: https://fast-spire-96062.herokuapp.com/
+url: https://cousinsd-shopify-boxes-dev.herokuapp.com/
 database: Created postgresql-reticulated-46490 as ``DATABASE_URL``
 
 Drop and recreate the database with ``pg:reset``. Push local database data with ``pg:push``.
 
 ```bash
-heroku pg:reset --confirm fast-spire-96062 DATABASE_URL
+heroku pg:reset --confirm cousinsd-shopify-boxes-dev DATABASE_URL
 PGUSER=cousinsd PGPASSWORD=******** heroku pg:push shopify_app DATABASE_URL
 ```
 
--- next.config.js
+++ next.config.js
@@ -6,16 +6,46 @@ const fetch = require('isomorphic-fetch');
 
 // next-css needed for polaris to load css files
 // XXX next now has another way of creating globals - catch up!
-//const withTM = require('next-transpile-modules')(['@cousinsd/shopify-boxes-client']);
-const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([
-  [
-    withCSS
-  ],
-  [
-    //    withTM(
-      {
+if (process.env.NODE_ENV === 'production') {
+
+  myexports = withCSS({
+    webpack: (config) => {
+      config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
+      config.resolve.alias['react-dom'] = path.resolve(__dirname, '.', 'node_modules', 'react-dom');
+      const env = { 
+        API_KEY: JSON.stringify(process.env.SHOPIFY_API_KEY),
+        API_VERSION: JSON.stringify(process.env.API_VERSION),
+        API_PASSWORD: JSON.stringify(process.env.SHOPIFY_API_PASSWORD),
+        SHOP_NAME: JSON.stringify(process.env.SHOP_NAME),
+        HOST: JSON.stringify(process.env.HOST),
+        LABELKEYS: JSON.stringify([
+          'Delivery Date', 
+          'Including', 
+          'Add on items', 
+          'Removed items', 
+          'Subscription',
+          'Add on product to'
+        ]),
+        REACT1: require('react'),
+        fetch: fetch,
+      };
+      config.plugins.push(new webpack.DefinePlugin(env));
+      return config
+    },
+  });
+
+} else {
+
+  const withTM = require('next-transpile-modules')(['@cousinsd/shopify-boxes-client']);
+  const withPlugins = require('next-compose-plugins');
+
+  myexports = withPlugins([
+    [
+      withCSS
+    ],
+    [
+      withTM({
         webpack: (config) => {
           config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
           config.resolve.alias['react-dom'] = path.resolve(__dirname, '.', 'node_modules', 'react-dom');
@@ -39,17 +69,9 @@ module.exports = withPlugins([
           config.plugins.push(new webpack.DefinePlugin(env));
           return config
         },
-      }
-    // )
-  ],
-]);
-/*
- * should be reading now from .env.local
-API_KEY: JSON.stringify(process.env.SHOPIFY_API_KEY),
-API_VERSION: JSON.stringify(process.env.API_VERSION),
-SHOP_ID: JSON.stringify(process.env.SHOP_ID),
-SHOP_PASSWORD: JSON.stringify(process.env.SHOP_PASSWORD),
-SHOP_USERNAME: JSON.stringify(process.env.SHOP_USERNAME),
-SHOP_NAME: JSON.stringify(process.env.SHOP_NAME),
-HOST: JSON.stringify(process.env.HOST),
-*/
+      })
+    ],
+  ]);
+};
+
+module.exports = myexports;
